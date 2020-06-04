22c015c2bbc397d49753e5e85b32d484451e8100
arai <>
2020-06-04 23:11:13 +0900

    問題の出力

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
-const question = [
+const questions = [
   "JavaScript",
   "split",
   "console.log",
   "document.getElementById"
 ];
-const getQ = document.getElementById("question");
+//配列questionsからランダムに取り出す
+const mathNum =  Math.floor(Math.random() * questions.length);
+const question = questions[mathNum];
 
+//一文字ずつ格納
+const questionText = [...question];
 
+//問題を出力
+const element = document.getElementById("question");
+element.insertAdjacentHTML("afterbegin",question)
+
+//押されたキーと問題テキストが一致した時
+console.log(questionText);
+document.addEventListener('keydown',event => {
+  if(event.key === questionText[0]){
+    console.log("true");
+    questionText.shift();
+  }
+});
