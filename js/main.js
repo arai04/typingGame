65dda5601bd385b9485522d6bab5a0fab5aab568
arai <>
2020-06-05 23:35:56 +0900

    ランダムに重複無しで出題

--- js/main.js
+++ js/main.js
@@ -4,23 +4,47 @@ const questions = [
   "JavaScript",
   "split",
   "console.log",
-  "document.getElementById"
+  "document.getElementById",
+  "shift",
+  "insertAdjacentElement",
+  "class",
+  "length"
 ];
+
+//questionのindex分乱数を格納
+const numQ = [];
+const numQrnd = [];
+for(let i = 0 ; i < questions.length ; i ++){
+  numQ.push(i);
+}
+const createNumRndUnique = () =>{
+  for(let i = numQ.length ; i > 0 ; i --){
+    const rnd = Math.floor(Math.random() * Math.floor(numQ.length));
+    const removed  = numQ.splice(rnd,1);
+    numQrnd.push(removed[0]);
+  }
+};
+createNumRndUnique();
+
 //配列questionsからランダムに取り出す
-const mathNum =  Math.floor(Math.random() * questions.length);
-const question = questions[mathNum];
 
 //一文字ずつspnaタグをつけて格納
 let questionText = [] ;
-questionText = [...question].map( (text) =>{
-  const spanElement = document.createElement('span');
-  spanElement.textContent = text ;
-  p.appendChild(spanElement);
-  //insertAdjacentElement('afterbegin',spanElement);だと
-  //HTMLに挿入される時鏡文字で挿入された。
-  //配列の中身は逆にならなかった
-  return spanElement;
-} );
+const createText = () => {
+  p.textContent = '';
+  const question = questions[numQrnd[0]];
+  numQrnd.shift();
+  questionText = [...question].map( (text) =>{
+    const spanElement = document.createElement('span');
+    spanElement.textContent = text ;
+    p.appendChild(spanElement);
+    //insertAdjacentElement('afterbegin',spanElement);だと
+    //HTMLに挿入される時鏡文字で挿入された。
+    //配列の中身は逆にならなかった
+    return spanElement;
+  } );
+}
+createText();
 
 //押されたキーと問題テキストが一致した時
 document.addEventListener('keydown',keyDown => {
@@ -28,5 +52,7 @@ document.addEventListener('keydown',keyDown => {
     console.log("true");
     questionText[0].className = "changeColor";
     questionText.shift();
+
+    if(questionText.length === 0) createText();
   }
 });
